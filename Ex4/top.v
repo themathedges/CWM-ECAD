971479896b3f1c1b0c2011c24e5aa7265d22012a
Cloud User <centos@ip-172-31-0-63.eu-west-2.compute.internal>
2020-06-16 14:24:28 +0000

    ex 4 in progress

--- Ex4/top.v
+++ Ex4/top.v
@@ -16,3 +16,58 @@
 //  You need to write the whole file.
 //////////////////////////////////////////////////////////////////////////////////
 
+module dice(input clk, rst, button, output [2:0] throw)
+
+always @(*)
+
+	if ((rst == 0) && (button || !button)) begin // im not entirely sure if im starting this correctly?
+	throw = 3'b1;
+
+		if (rst == 1)
+		?
+		else if ((rst == 0) && (!button))
+		throw = throw;
+		else if 
+		throw = 3'b10;        
+        
+		if (rst == 1)
+		?
+		else if ((rst == 0) && (!button))
+		throw = throw;
+		else if 
+		throw = 3'b11;
+
+		if (rst == 1)
+		?
+		else if ((rst == 0) && (!button))
+		throw = throw;
+		else if
+		throw = 3'b100;
+
+		if (rst == 1)
+		?
+		else if ((rst == 0) && (!button))
+		throw = throw;
+		else if
+		throw = 3'b101;
+
+		if (rst == 1)
+		?
+		else if ((rst == 0) && (!button))
+		throw = throw;
+		else
+		throw = 3'b110;
+
+		if (rst == 1)
+		?
+		else if (!button)
+		throw = throw;
+		else if
+		throw = 3'b1;
+	else
+	?						// im not sure what to write if rst == 1?
+	end
+	
+endmodule
+
+
