890bca4f4571389d0e97d94db4e488ca57469997
Cloud User <centos@ip-172-31-0-63.eu-west-2.compute.internal>
2020-06-16 15:40:44 +0000

    testbench in progress

--- Ex4/top_tb.v
+++ Ex4/top_tb.v
@@ -6,4 +6,47 @@
 // Description: A testbench module to test Ex4 - Electronic Dice
 // You need to write the whole file
 //////////////////////////////////////////////////////////////////////////////////
+`timescale 1ns/100ps
 
+module testbench();
+
+parameter clk_period = 10;
+
+reg clk, rst, button, err;
+reg [2:0] tmp;
+wire [2:0] throw;
+
+initial begin
+	clk = 0;
+	forever
+		#(clk_period/2)clk = ~clk; 		// delay of half a cycle, clk = bitwise not clk 
+	end
+
+initial begin						// initialise values
+	rst = 0;
+	#clk_period rst = 1; 
+	button = 0;      
+	#clk_period rst = 0;            			
+	#clk_period button = 1;
+	err = 0;
+	#(4*clk_period)
+
+	forever begin
+		tmp <= throw;
+		#(clk_period) 
+		if (tmp != throw + 1);
+		$display("TEST FAILED");
+		err = 1;
+		end
+end
+
+initial begin
+	#(20*clk_period)
+	if (err == 0)
+	$display("TEST PASSED");				
+	$finish;
+	end 
+
+dice dice(.clk(clk),.rst(rst),.button(button),.throw(throw)); 		// instantiate module
+
+endmodule 
