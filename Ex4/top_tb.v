109f4c12d42032e56a0b9cadd4ef44b8f922edbf
Cloud User <centos@ip-172-31-0-63.eu-west-2.compute.internal>
2020-06-17 08:38:23 +0000

    ex4

--- Ex4/top_tb.v
+++ Ex4/top_tb.v
@@ -23,19 +23,19 @@ initial begin
 	end
 
 initial begin	
-	tmp = 3'b0;					// initialise values
-	rst = 0;
-	#clk_period rst = 1; 
-	button = 0;      
+	//tmp = 3'b0;					// initialise values
+	//rst = 0;
+	//#clk_period rst = 1; 
+	//button = 0;      
 	#clk_period rst = 0;            			
 	#clk_period button = 1;
 	err = 0;
-	#(10*clk_period)
+	//#(2*clk_period)
 
 	forever begin
 		tmp = throw;
-		#(clk_period) 
-		if (tmp != throw + 1); // this may need to be the other way around
+		#(2*clk_period) // not sure but think it is 2 periods for throw to change
+		if ((throw != tmp + 1) && (throw != 3'b1)); // this may need to be the other way around it could be failing because it goes back to the begin ie not tmp + 1
 		$display("TEST FAILED");
 		err = 1;
 		end
