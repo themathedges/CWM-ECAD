14b33d14e768ab68203fde5ecf344ef419017a3b
Cloud User <centos@ip-172-31-0-63.eu-west-2.compute.internal>
2020-06-17 15:52:25 +0000

    ex5

--- Ex5/top.v
+++ Ex5/top.v
@@ -12,4 +12,31 @@
 //  outputs:
 //           red, amber, green
 //////////////////////////////////////////////////////////////////////////////////
+`timescale 1ns/100ps
+
+module traffic(input clk, output red, amber, green);
+
+reg [2:0] state;
+
+assign red = state[2];
+assign amber = state[1];
+assign green = state[0];
+
+always @(posedge clk) begin // [111] represents RAG
+
+			if (state == 3'b100)
+			state <= 3'b110;      
+        
+			else if (state == 3'b110)
+			state <= 3'b001;
+		
+			else if (state == 3'b001)
+			state <= 3'b010;
+		
+			else //if (state == 3'b010)
+			state <= 3'b100;
+						
+end
+	
+endmodule
 
