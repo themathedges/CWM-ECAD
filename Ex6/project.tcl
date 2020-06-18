36f86288033bcf46c4a4b3c237e353d3b1e4a33f
Cloud User <centos@ip-172-31-0-63.eu-west-2.compute.internal>
2020-06-18 19:49:31 +0000

    update Ex6 project.tcl

--- Ex6/project.tcl
+++ Ex6/project.tcl
@@ -21,6 +21,8 @@ create_fileset -constrset -quiet constraints
 #set_property is_enabled false [get_files ${project_constraints}]
 
 #Todo: add verilog modules here
+read_verilog "traffic.v"
+read_verilog "dice.v"
 read_verilog "top.v"
 read_verilog "top_tb.v"
 
