941abf78be84355767e5ad62c4cc6c1832f14887
Cloud User <centos@ip-172-31-0-63.eu-west-2.compute.internal>
2020-06-18 16:49:25 +0000

    testbench in progress

--- Ex7/top_tb.v
+++ Ex7/top_tb.v
@@ -6,4 +6,45 @@
 // Description: A testbench module to test Ex7 - Times Table
 // You need to write the whole file
 //////////////////////////////////////////////////////////////////////////////////
+`timescale 1ns/100ps
+module testbench();
 
+parameter clk_period = 10;
+reg clka, ena, err; 
+reg [2:0] a; 
+reg [2:0] b;
+wire [5:0] result;
+
+initial begin
+	clka = 0;
+	forever begin
+	#(clk_period/2)clka = ~clka; 
+	end		
+	end
+
+initial begin
+	ena = 1;	 
+	err = 0;
+	a = 3'b011; // 3 * 4 should give result = 12
+	b = 3'b100;					
+	#clk_period
+	forever begin
+		#clk_period 			
+		if ((a*b != result) && ena) begin	
+		$display("TEST FAILED");
+		err = 1;
+		end
+		end
+		end
+
+initial begin
+	#(20*clk_period)
+	if (err == 0) begin
+	$display("TEST PASSED");				
+	$finish;
+	end
+	end
+
+multiplier multiplier(.clka(clka), .a(a), .b(b), .ena(ena), .result(result)); 
+
+endmodule
