8e7e689b818421a49e9a1d27f2c70cf0277aed11
Cloud User <centos@ip-172-31-0-63.eu-west-2.compute.internal>
2020-06-18 16:02:05 +0000

    ex 7 in progress

--- Ex7/top.v
+++ Ex7/top.v
@@ -10,7 +10,30 @@
 //           clk, a[2:0], b[2:0], read
 //
 //  outputs:
-//           result[4:0]
+//           result[5:0] 
 //////////////////////////////////////////////////////////////////////////////////
 
+`timescale 1ns/100ps
+module multiplier(clka, a, b, ena, result);
 
+input clka, ena, [2:0] a, [2:0] b;
+output reg [5:0] result
+
+always @(posedge clka) begin
+	if (ena) begin
+	a
+	
+	end else begin
+	result <= 5'b0;
+	end end
+
+blk_mem_gen_0 mybram (
+  .clka(clka),    // input wire clka
+  .ena(ena),      // input wire ena
+  .wea(wea),      // input wire [0 : 0] wea
+  .addra(addra),  // input wire [5 : 0] addra
+  .dina(dina),    // input wire [5 : 0] dina
+  .douta(douta)  // output wire [5 : 0] douta
+);
+
+endmodule
