032fed71e5dcabe86ff875016a4024745342d62b
Cloud User <centos@ip-172-31-0-63.eu-west-2.compute.internal>
2020-06-19 14:54:53 +0000

    top

--- Ex8/top.v
+++ Ex8/top.v
@@ -14,15 +14,19 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 `timescale 1ns/100ps
-module multiplier(input clk, input rst, input [2:0] a, input [2:0] b, input read, output wire [5:0] result);
+module multiplier(input clk, input rst, input [2:0] a, input [2:0] b, input read, output reg [5:0] result);
 
 wire [31:0] addr_wire;
 wire [31:0] result_wire;
+wire arvalid, rvalid, rsta_busy, rstb_busy;
 
-always @(*) begin
-		result_wire = {26'b0,result};
-		addr_wire = {24'b0,a,b,2'b0};
-end
+assign addr_wire = arvalid ? {24'b0,a,b,2'b0} : 32'bX; 	// undefined if !arvalid
+
+assign rsta_busy = rst? 1'b1:1'b0;
+assign rstb_busy = rst? 1'b1:1'b0;
+
+assign result_wire[5:0] = (!rst && rvalid) ? result:result_wire[5:0]; // undefined if !rvalid
+
 
 // . instance port ( module assignment ) is notation
 
@@ -30,7 +34,7 @@ blk_mem_gen_0 mybram (
   .rsta_busy(rsta_busy),          // output wire rsta_busy
   .rstb_busy(rstb_busy),          // output wire rstb_busy
   .s_aclk(clk),                // input wire s_aclk
-  .s_aresetn(rst),          // input wire s_aresetn ~ IS THIS !RST ??		
+  .s_aresetn(rst),          // input wire s_aresetn ~ *** IS THIS !RST ??		
   .s_axi_awaddr(32'b0),    // input wire [31 : 0] s_axi_awaddr
   .s_axi_awvalid(1'b0),  // input wire s_axi_awvalid
   .s_axi_awready(),  // output wire s_axi_awready
@@ -42,11 +46,11 @@ blk_mem_gen_0 mybram (
   .s_axi_bvalid(),    // output wire s_axi_bvalid
   .s_axi_bready(1'b1),    // input wire s_axi_bready ~ HIGH IE READY TO READ ??
   .s_axi_araddr(addr_wire),    // input wire [31 : 0] s_axi_araddr
-  .s_axi_arvalid(1'b1),  // input wire s_axi_arvalid
-  .s_axi_arready(),  // output wire s_axi_arready
+  .s_axi_arvalid(arvalid),  // input wire s_axi_arvalid
+  .s_axi_arready(1'b1),  // output wire s_axi_arready
   .s_axi_rdata(result_wire),      // output wire [31 : 0] s_axi_rdata
   .s_axi_rresp(),      // output wire [1 : 0] s_axi_rresp
-  .s_axi_rvalid(),    // output wire s_axi_rvalid
+  .s_axi_rvalid(rvalid),    // output wire s_axi_rvalid
   .s_axi_rready(1'b1)    // input wire s_axi_rready
 );
 
