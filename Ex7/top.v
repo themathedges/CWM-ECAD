5e32958c31c1dff63473863e1e6bf440949afb1e
Cloud User <centos@ip-172-31-0-63.eu-west-2.compute.internal>
2020-06-19 12:08:41 +0000

    ex7

--- Ex7/top.v
+++ Ex7/top.v
@@ -14,27 +14,17 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 `timescale 1ns/100ps
-module multiplier(input clka, input [2:0]a, input [2:0] b, input ena, output reg [5:0] result);
+module multiplier(input clk, input [2:0] a, input [2:0] b, input read, output wire [5:0] result);
 
-reg [5:0] addr1;
-reg [5:0] dout1;
-
-always @(posedge clka) begin
-	if (ena) begin
-	addr1 <= a*b;
-	result <= dout1;
-	
-	end else begin
-	result <= 5'b0;
-	end end
+// . instance port ( module assignment ) is notation
 
 blk_mem_gen_0 mybram (
-  .clka(clka),    // input wire clka
-  .ena(ena),      // input wire ena
+  .clka(clk),    // input wire clka
+  .ena(read),      // input wire ena
   .wea(wea),      // input wire [0 : 0] wea
-  .addra(addr1),  // input wire [5 : 0] addra
-  .dina(dina),    // input wire [5 : 0] dina
-  .douta(dout1)  // output wire [5 : 0] douta
+  .addra({a,b}),  // input wire [5 : 0] addra
+  .dina(6'b0),    // input wire [5 : 0] dina
+  .douta(result)  // output wire [5 : 0] douta
 );
 
 endmodule
