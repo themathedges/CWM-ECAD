61906725ad44b8495dbf05078995b9f8f29b7f0f
Cloud User <centos@ip-172-31-0-63.eu-west-2.compute.internal>
2020-06-19 12:50:21 +0000

    testbench in progress

--- Ex8/top_tb.v
+++ Ex8/top_tb.v
@@ -7,3 +7,37 @@
 // You need to write the whole file
 //////////////////////////////////////////////////////////////////////////////////
 
+`timescale 1ns/100ps
+module testbench();
+
+parameter clk_period = 10;
+reg clk, read, rst; 
+reg [2:0] a; 
+reg [2:0] b;
+wire [5:0] result;
+
+initial begin
+	clk = 0;
+	forever begin
+	#(clk_period/2)clk = ~clk; 
+	end		
+	end
+
+initial begin
+	rst = 1;
+	#(3*clk_period)
+	rst = 0;
+	read = 0;	 
+	a = 3'b011; // 3 * 4 should give result = 12 ie 1100
+	b = 3'b100;					
+	#clk_period
+	read = 1;
+	forever begin
+		#clk_period 				
+		$display("a=%d b=%d result=%d", a, b, result); // display result in decimal
+		end
+		end
+
+multiplier multiplier(.clk(clk), .rst(rst), .a(a), .b(b), .read(read), .result(result)); 
+
+endmodule
