c596e383db78b90f01ad64c8928df9bc3ea28807
Cloud User <centos@ip-172-31-0-63.eu-west-2.compute.internal>
2020-06-19 12:09:53 +0000

    ex7

--- Ex7/top_tb.v
+++ Ex7/top_tb.v
@@ -10,41 +10,30 @@
 module testbench();
 
 parameter clk_period = 10;
-reg clka, ena, err; 
+reg clk, read; 
 reg [2:0] a; 
 reg [2:0] b;
 wire [5:0] result;
 
 initial begin
-	clka = 0;
+	clk = 0;
 	forever begin
-	#(clk_period/2)clka = ~clka; 
+	#(clk_period/2)clk = ~clk; 
 	end		
 	end
 
 initial begin
-	ena = 1;	 
-	err = 0;
-	a = 3'b011; // 3 * 4 should give result = 12
+	read = 0;	 
+	a = 3'b011; // 3 * 4 should give result = 12 ie 1100
 	b = 3'b100;					
 	#clk_period
+	read = 1;
 	forever begin
-		#clk_period 			
-		if ((a*b != result) && ena) begin	
-		$display("TEST FAILED");
-		err = 1;
-		end
+		#clk_period 				
+		$display("a=%d b=%d result=%d", a, b, result); // display result in decimal
 		end
 		end
 
-initial begin
-	#(20*clk_period)
-	if (err == 0) begin
-	$display("TEST PASSED");				
-	$finish;
-	end
-	end
-
-multiplier multiplier(.clka(clka), .a(a), .b(b), .ena(ena), .result(result)); 
+multiplier multiplier(.clk(clk), .a(a), .b(b), .read(read), .result(result)); 
 
 endmodule
